41990bc2081e0262500b9a8d9c5bc9b06f5e30a1
LoliweLoliwe <109585257+LoliweLoliwe@users.noreply.github.com>
2023-08-27 09:49:47 +0100

    Up to 20 words

--- index.js
+++ index.js
@@ -81,7 +81,68 @@ app.get('/table', (req, res) => {
             term: 'Bootstrap',
             description: 'Is a program/code that works with a small amount of extra code as it comes in a pre-arranged format.',
             reference: 'Klein, M. (2021). A Beginner’s Guide to Bootstrap. Available at: https://www.codecademy.com/resources/blog/what-is-bootstrap/#:~:text=As%20a%20framework%2C%20Bootstrap%20is%20a%20collection%20of,to%20create%20every%20bit%20of%20code%20from%20scratch.'
-        }];
+        },
+        {
+            number: '11',
+            term: 'Continuous integration',
+            description: 'is the practice of automating the integration of code changes from multiple contributors into a single software project.',
+            reference: 'Rehkopf. M. (n.d.). What is continuous integration? Available at: https://www.atlassian.com/continuous-delivery/continuous-integration.'
+        },
+        {
+            number: '12',
+            term: 'Continuous delivery',
+            description: 'is an approach where teams release products frequently and with high quality, and with a level of predictability from source code repositories through to a production environment using automation.',
+            reference: 'Coupland, M. (2021), DevOps Adoption Strategies: Principles, Processes, Tools and Trends. Packt Publishing.'
+        },
+        {
+            number: '13',
+            term: 'Deployment',
+            description: 'means pushing changes or updates from one deployment environment to another.',
+            reference: 'Umbraco (n.d). What is Deployment? Available at: https://umbraco.com/knowledge-base/deployment/'
+        },
+        {
+            number: '14',
+            term: 'Kanban',
+            description: 'is a workflow management method that helps organizations manage and improve work systems.',
+            reference: 'Kanbaniza. (n.d.). What Is Kanban? Explained for Beginners. Available at: https://kanbanize.com/kanban-resources/getting-started/what-is-kanban'
+        },
+        {
+            number: '15',
+            term: 'Docker',
+            description: 'is a complete solution for the production, distribution, and use of containers.',
+            reference: 'Walker T. (2021). Docker for Beginners: Everything You Need to Know. Available at: https://www.howtogeek.com/733522/docker-for-beginners-everything-you-need-to-know/'
+        },
+        {
+            number: '16',
+            term: 'Kubernetes',
+            description: 'is an open-source system for automating deployment, scaling, and management of containerized applications.',
+            reference: 'The Kubernetes Authors. (n.d.). Production-Grade Container Orchestration. Available at: https://kubernetes.io/'
+        },
+        {
+            number: '17',
+            term: 'Acceptance Test',
+            description: 'Gillis, A.S. (2021). Acceptance Testing. Available at: https://www.techtarget.com/searchsoftwarequality/definition/acceptance-test.',
+            reference: 'Gillis, A.S. (2021). Acceptance Testing. Available at: https://www.techtarget.com/searchsoftwarequality/definition/acceptance-test.'
+        },
+        {
+            number: '18',
+            term: 'Configuration Test',
+            description: 'is a software testing technique in which the software application is tested with multiple combinations of software and hardware in order to evaluate the functional requirements and find out optimal configurations under which the software application works without any defects or flaws.',
+            reference: 'Guru99. (n.d.). What is Configuration Testing? Example Test Cases. Available at: https://www.guru99.com/configuration-testing.html'
+        },
+        {
+            number: '19',
+            term: 'Patch',
+            description: 'A software patch is a piece of code that updates an existing computer program by addressing known bugs or vulnerabilities within the system.',
+            reference: 'Farrelly, J. (2022). What is a Patch in Computer Terms? Available at: https://www.electric.ai/blog/what-are-patches-in-computer'
+        },
+        {
+            number: '20',
+            term: 'Staging',
+            description: 'is an environment for computer people to test and view their application and changes before they go live to the world (production).',
+            reference: 'CommonPlaces. (n.d.). What is Staging? Available at: https://www.commonplaces.com/blog/web-development-what-is-staging/'
+        }
+    ];
 
     // Render method takes two parameter
     // first parameter is the ejs file to 
